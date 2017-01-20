de531de331f08bc69ecc0e9ad5fdc6a676e9eb80
Frank Cognata <francocognata@gmail.com>
2017-01-20 14:15:23 -0500

    tstat

---
+++ devicetypes/frankcog/ki-zigbee-thermostat.src/ki-zigbee-thermostat.groovy
@@ -0,0 +1,57 @@
+/**
+ *  Ki-ZigBee-Thermostat 
+ *
+ *  Copyright 2017 Frank Cognata
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Ki-ZigBee-Thermostat ", namespace: "frankcog", author: "Frank Cognata") {
+		capability "Thermostat Fan Mode"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'thermostatFanMode' attribute
+
+}
+
+// handle commands
+def fanOn() {
+	log.debug "Executing 'fanOn'"
+	// TODO: handle 'fanOn' command
+}
+
+def fanAuto() {
+	log.debug "Executing 'fanAuto'"
+	// TODO: handle 'fanAuto' command
+}
+
+def fanCirculate() {
+	log.debug "Executing 'fanCirculate'"
+	// TODO: handle 'fanCirculate' command
+}
+
+def setThermostatFanMode() {
+	log.debug "Executing 'setThermostatFanMode'"
+	// TODO: handle 'setThermostatFanMode' command
+}
